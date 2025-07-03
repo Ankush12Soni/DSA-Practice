62af8de70e8b1bb5fde02c694a148765372a75a7
Ankush Soni <ankushsoni454545@gmail.com>
2025-07-03 17:53:51 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+
+public class BinarySearch {
+    public static void main(Pattern[] args) {
+        int[] arr = {1, 4, 5, 6, 10, 12, 25, 26, 30, 45};
+        int target = 26;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start);
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+

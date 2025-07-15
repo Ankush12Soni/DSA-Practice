3618b27d051a12deb17a99eb51234b078a71d88f
Ankush12Soni <ankushsoni454545@gmail.com>
2025-07-15 13:06:42 +0530

    Initial commit of DSA practice problems

--- Inheritance/Box.java
+++ Inheritance/Box.java
@@ -1,4 +1,21 @@
 package Inheritance;
 
 public class Box {
+    double l;
+    double h;
+    double w;
+
+    Box() {
+        this.h = -1;
+        this.l=-1;
+        this.w=-1;
+    }
+
+    Box(double side){
+        this.h=side;
+        this.l=side;
+        this.w=side;
+    }
+
 }
+
---
+++ Inheritance/Main.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+//        Box box = new Box(4);
+//        System.out.println(box.l +" "+ box.w+" "+ box.h+" ");
+
+        BoxWeight box = new BoxWeight();
+        System.out.println(box.weight+" "+box.h+" ");
+    }
+
+
+}
